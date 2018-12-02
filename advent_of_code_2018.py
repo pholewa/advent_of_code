74c6bc170b6eff07cb4c0f171309f698cfd7a784
pholewa <piotr2393@gmail.com>
2018-12-02 14:49:46 +0100

    Two days accomplished

---
+++ advent_of_code_2018.py
@@ -0,0 +1,321 @@
+import _collections
+input_data = """kqzxdenujwcsthbzgvyioflcsp
+vqwxdenujwcsthbmggyioflarp
+kqzxienujwcsthbmglyioclarp
+kuzxdetujwcsthbmgvyioflcrp
+kqnxdenujwcsthbmgvlooflarp
+kqzxdknpjwcsthwmgvyioflarp
+kgzxdenujwcsthbfgvyicflarp
+kqzxdenujrnsthbmgjyioflarp
+lqzxdeeujwcsthbmrvyioflarp
+iqfxdenujwcsthbmgvyiofyarp
+kvzxbenujwcstabmgvyioflarp
+kmzxdenujwcsthbmglyioolarp
+kqzxdenujhcsthbmgbyioflanp
+nqzxdenujwcsehbmgvsioflarp
+kqzlgenujwcsthbmgvyiofjarp
+kqzxdyfujwcsihbmgvyioflarp
+kqzxdsnujwcqthbmgvyiorlarp
+kqzxdenuywcsthbmgvyinflmrp
+knzxderujwcsthbmgvyioflaop
+kqxxdenujwczthbmgvyioflajp
+kqzxdevujwcsthbmgvyqoxlarp
+kqzxdenujwclmhbmgvyioslarp
+kqzldenujwcsthbmgvnisflarp
+kjtxdenujwcsthbmgvyfoflarp
+kqzxwenujwcstxbmgvyihflarp
+kqzxdenuhecsthbmgvyeoflarp
+kqzxdenhjwesthbmgvyioklarp
+kqkxdenujwcsthbcgvyiofgarp
+kqyxmenujwcsthbmgvyioflara
+kqzxdqnrjwcwthbmgvyioflarp
+kqzxdenufwcgyhbmgvyioflarp
+lqzxdenujwcsthbmtvyiofearp
+kqzxdenujwcsthbvgvthoflarp
+kqzxeenujwcsahbmgvyioflamp
+pqzxdenujwcsshbmjvyioflarp
+kqzxdesujwcstdbmgvyioflatp
+kqzxpenujwcsthimgvyioflhrp
+kqzxdmkujwcsthbmgvpioflarp
+kszxdenujwcsthbybvyioflarp
+kqzxdvnujwcsthbmgvyqoslarp
+kkzxdetujwcsthbmgvyiofltrp
+kqzxdenujwcsthomgvyiozlaro
+cqzfdenubwcsthbmgvyioflarp
+kqzxdenyjwcsthbmhvyiofldrp
+kqzxdenujwcsthbmghfiofxarp
+kmqxdebujwcsthbmgvyioflarp
+kqzxdenufwcsthbmvvypoflarp
+kqnxdenujwcsthbmgvtzoflarp
+bqzxdenujwcithbmgvyiohlarp
+kqzxdenurwrsthbmgvyioelarp
+kqzxdenujwcsthbmgpyiodlarl
+kqzxdengjwcxthbmgvjioflarp
+kizxdenujwcsnhqmgvyioflarp
+jqzxdenajwcsthbmnvyioflarp
+kqzcdenujwcsphbigvyioflarp
+kezxdenujwcsthbfgvyioflaqp
+kqzxdenujwcstybmgvyivfyarp
+kqzxdenujwcsthbmgvbiofsnrp
+kqzxdenujwcsthbmgvyhxfnarp
+kvzxdenqjfcsthbmgvyioflarp
+kqzxywnljwcsthbmgvyioflarp
+kqzxdenujwcsbhbzgvyioxlarp
+kqkxdenufwcsthbmgvyiofxarp
+dqzxddnujwcsthsmgvyioflarp
+yqrxdenujwcsthbagvyioflarp
+kqzxdenujwcsajbmgvyiovlarp
+kqztdunujwcsthbmgvyioilarp
+kqzxdequjwcsthbmgvyyoflarm
+kqzxdlnujwksthbmgvkioflarp
+tqvxdenujwcsthbmgveioflarp
+kqzndezupwcsthbmgvyioflarp
+kqzctsnujwcsthbmgvyioflarp
+kqzxdenujwmstkbmgvyioflgrp
+kqzxdenujwzsthdmgvyiofdarp
+kqzxdynujwcsthcmgvyioflasp
+kqzxdesujwcstybmgcyioflarp
+kqzxdenujwcsthbvgvyiyglarp
+kqzxpenujwcsthbogvyioflard
+khzxdenujwcsthbmgvyikflaep
+kqzxdedujwchthbmgvyeoflarp
+kxzxsepujwcsthbmgvyioflarp
+xqzxdenujwcsthbpgvyioelarp
+jfzxdenujwcsthbmgvyiollarp
+kqzxcenujwcethbmgvwioflarp
+kqzxdenujwcithbmgvysoflarg
+kqlxdenujwnsthbmgvyiotlarp
+wqzydenujwcsthbmgvyioftarp
+kqzxienuwwcsthbmgayioflarp
+kqzxdetujwcsthbmgvyhoflawp
+kqzxdqnujwrsthbmgvyxoflarp
+kqzxdenujwcvthbmgjiioflarp
+kqzxdenujwcjthbxgvaioflarp
+kqzxpenujwcsthymgvyioklarp
+kqzxdenujwcsthbmswygoflarp
+kqzxdenujwcsthbmgvyiaxiarp
+kqzxdenudkcsthbmgvyzoflarp
+kqzxdvndjwcsthbmgvyioflaxp
+kqzxdenujwcsthbmdvymoflvrp
+kqzxvenujwcsihbmgvyiofllrp
+kqzxdqnujwcsthbmgtyioflprp
+kqzxdenuuwcathbmgvsioflarp
+kqzrdenujwesthbjgvyioflarp
+kqzxdexujwcstzbmgvyyoflarp
+kqzxpenujwjstabmgvyioflarp
+kozxdenejwcsthbmgvpioflarp
+kbzxdenvjwcsthbmgvyiofsarp
+kolxdenujwcjthbmgvyioflarp
+kqzxdenujwcsthbmgvyiffsakp
+kqzxdelujwcsthbmlvyioflxrp
+kqzxdenugwcsthrmgvyioflprp
+kqzxdelujwcsthqmgvyiozlarp
+kqzxienujwosthbmgvykoflarp
+kqzxdeuujwicthbmgvyioflarp
+kqzxdenbjwcsthbmcvyaoflarp
+krzxdqnujwcsthbmgvyioflerp
+wqzxzenujwcsthbmgvyioclarp
+kqzxyenujwcsthbmgejioflarp
+kqzxdenujwcstsbmgvtidflarp
+kqnxdenejwcsthbmgvyioflara
+kqzsdmnujwcsthbmgvyioflaep
+kqzxdedujwnsthymgvyioflarp
+kqzxdenujwusthbmgnyioflarx
+kqzxlenujwcsthbmgayvoflarp
+kqzxdenujwcsthbmgvyiofngrh
+zqzxdenujwcsthbmgvyiofvxrp
+kqzydenujwmsthbmgvyiuflarp
+kqzxdenujkrsthbmdvyioflarp
+kqzxdlnujocsthbmgvyiofaarp
+kqzxdenujwcstybmgvyiofrwrd
+kqzxdenupwksthbmgvyiofbarp
+khzxdentjwcsthbmbvyioflarp
+kqzxdenujwcuphbmgvyihflarp
+kqzxdenhjwcgthbmgvyioflrrp
+kqzxdenujwcsthbmgvyiofakhp
+kqzxdenujwcstfkmgvyioflamp
+kqzxdenujqcsthbmgvkiorlarp
+kqzxdenujwcstvbmgvyioilasp
+kqzxdxnujwcsthbpgayioflarp
+kqzxdenupwysthbmgvyiofljrp
+kqzxdenujwcdthbmgvymoflarv
+kqnxdenujwcstvbmgvyixflarp
+kqjxdenujwcsthbmgvyikflurp
+kqsxdenulwcsthxmgvyioflarp
+bqzxbenujwcsahbmgvyioflarp
+vqzxdenujwcsthbmgvjzoflarp
+kqzhfenujwcsthimgvyioflarp
+eqzxdenujwcshhbmgnyioflarp
+kqzxdenujucstubmgvyicflarp
+kuzxdenuewcsthbmgvyiofuarp
+kqzxdenulwcsthbmgpyigflarp
+kqzxdebujwcsthbmgoyioflaro
+kqzxdenujwcuthbmgucioflarp
+kqzxdenujwcschpmgvyioflhrp
+kqzxfenujwcsthbmjvrioflarp
+kqzxdenujqcsthbmgvyndflarp
+kqzxdgnbjwcsthbmgvywoflarp
+kqzxdenujwcsthrmgtbioflarp
+yqzxdenyjwcsthbmgvyioflarg
+kqzxdenuxwxsthbmsvyioflarp
+kqzxdenujwcsthbugqyvoflarp
+qqzxdenujwcsahbmgoyioflarp
+kqsxdenudwcsthbmguyioflarp
+kqzxdenujwcstublgvyioflamp
+kqzxdemujwtstqbmgvyioflarp
+kqzxqvnajwcsthbmgvyioflarp
+kqzxoennjwcstbbmgvyioflarp
+kqzxfenujwcsthbmlvyioflwrp
+kqzjdunujwcsthhmgvyioflarp
+kqzxdenujwcqthbmgvyirfxarp
+kqzxdengjwcsthbmgvyiowlgrp
+kqgxdenujwcswhbmglyioflarp
+mqzxdekuuwcsthbmgvyioflarp
+kqzxdenujwdsthbmgbyiovlarp
+krzxdenlhwcsthbmgvyioflarp
+kqzxdenmjwcstqbmgvyioflanp
+kqzxdenujwcmthbmgvtioflyrp
+kqzxdenujwcsthbmgvaijflprp
+kqzxdenuywysqhbmgvyioflarp
+kqzxdenujwfsthbmgvyhoflark
+nqzcdefujwcsthbmgvyioflarp
+kqzxdenujrcsthgmgyyioflarp
+kqzxdqnujwzsthbmgvyioftarp
+kqzxdenujwcsthimgvyioolapp
+kqzxdenupwcsthbmggyioflyrp
+kqzxdjnujwcsthbvgvyioflarf
+kqzxdtnujwasthbmgvyiofuarp
+kqzxbensjzcsthbmgvyioflarp
+kqzxdenujwcsphbmwiyioflarp
+kqzgdenuowcsthbmgvyioflarh
+kmzxdenujwasthbmgvtioflarp
+kqzxdenujwcstybmgvyiofrard
+vqzxdenejwcsthbmglyioflarp
+kqhxdenujwcsmhbmgvyioflprp
+kqzxdnnujwcsthzsgvyioflarp
+kczxdenujwcsthbmgvyeoflaop
+kqzxdenujwcsxhbmgvaioflaap
+kqzxdenujwcsthbmgayiofnprp
+kqzxdvnujwcsthbmgvyipjlarp
+kqzxdenubwcskhbmgvyiofkarp
+kqzxdenujwcsthbgggyigflarp
+kqzxdenujncstabvgvyioflarp
+kqzxdenujwcstqimqvyioflarp
+kqzxeenujwcsdhbmgvyqoflarp
+kcpxdenujwcsthbmgvyioilarp
+kqwxuenujwcsthbmgvyiyflarp
+kqzxdwnujwcstgbmgvyioplarp
+kqzxdenuswcstvbmglyioflarp
+kqzxdenujwcsthabgvyiwflarp
+kqzxdpnujwcsthbmwvyiomlarp
+kqzxdenujwcdthbmgvcioffarp
+kqzxdenajwcsthbmtvyiofldrp
+kqzbnenujwcshhbmgvyioflarp
+kqzbdequiwcsthbmgvyioflarp
+kqzxdenuswcsohbmgzyioflarp
+kvzxdenujwcstdbmjvyioflarp
+kqzxoenujwcqthbmpvyioflarp
+kqzxhenujwcsthbmgoyiofoarp
+klzxdenujwczthbmgvyioflanp
+kqpxdenujwcsthbmgvyioflafz
+kqkxdenujwcstxbngvyioflarp
+kqzepenuxwcsthbmgvyioflarp
+bqzxdenujmcithbmgvyioflarp
+kdzxdjnujwcstnbmgvyioflarp
+kszxdenujwcsthbmgeyiofrarp
+kqzxdenijwcsthbmgvhiaflarp
+kqzadenujwcbtxbmgvyioflarp
+kqkxwenujwcsthbmgvyiowlarp
+pqzddenujwcsthbmgvyboflarp
+kqzxxenujwcsthbwgvyioflmrp
+kqzxdjnujwcsthbmgvyipilarp
+pqzxdenujwcsthbmgvyieflark
+sqzxdenujtcsthbmgiyioflarp
+kqzxdznujwcsthbmgvzioflajp
+kqzxdrnujqcsthbmgvyiofvarp
+gqzxdenujwcsthemgvlioflarp
+kqzxdenujjcsthbmgvuiofljrp
+kqzsdenujmcsthbmggyioflarp
+kqzxienujwcsthbmgvaioflaip
+kqzxdwnujwcstfkmgvyioflarp
+kqzqdenujwcithbmzvyioflarp
+kqzxdedpjwcsthbmgvyiofbarp
+kqzxdeaujwcbtdbmgvyioflarp
+kqzewenyjwcsthbmgvyioflarp
+kqzxddnujwcsthbmgyyiofrarp
+kqzxdtnujwcsthbmgvyiodlard
+kqzxdefujwcsthbmgvyiffwarp
+xczxdenujwcsthbmgvyooflarp
+kuzxdenujucsthbmgvykoflarp
+kqzxtenujwcwthbmgvyioplarp
+kqzxdencllcsthbmgvyioflarp"""
+
+test_data = """abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab"""
+
+
+
+def day1(data):
+    a = list(map(int, data.split('\n')))
+    print(sum(a))
+
+def day1_2(data): #to improve, try to use set
+    value = 0
+    tmp_list = []
+    d = _collections.deque(list(map(int, data.split('\n'))))
+
+    while value not in tmp_list:
+        tmp_list.append(value)
+        value += d[0]
+        d.rotate(-1)
+
+    print(value)
+
+def day2(data):
+    a = []
+    two_times, three_times = 0, 0
+    for i, line in enumerate(data.splitlines()):
+        a.append(set(list(line)))
+        if len(a[i]) == len(line)-1:
+            two_times += 1
+        if len(a[i]) < len(line)-1:
+            item_counter = []
+            has_three, has_two = False, False
+            for j, item in enumerate(list(set(line))):
+                item_counter.append((item, line.count(item)))
+                if has_three == False and item_counter[j][1] == 3:
+                    three_times += 1
+                    has_three = True
+                elif has_two == False and item_counter[j][1] == 2:
+                    two_times += 1
+                    has_two = True
+                elif has_two == True and has_three == True:
+                    break
+    print(two_times * three_times)
+
+def day2_1(data):
+    a = []
+    result = ''
+    for i, line in enumerate(data.splitlines()):
+        letters = list(line)
+        a.append(letters)
+    for i in range(len(a)):
+        for j in range(1, len(a)):
+            z = [x for x, y in zip(a[i], a[j]) if x == y]
+            if len(z) == len(a[i]) - 1:
+                result = ''.join(z)
+                print(result)
+            if result != '':
+                break
+
+
+
+
+
+if __name__ == "__main__":
+    day2_1(input_data)
