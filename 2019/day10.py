4198373b36973ae6c0684b62eae71d4aaf1ca8f1
pholewa <piotr2393@gmail.com>
2019-12-12 22:06:00 +0100

    UPDATED day10 solution

--- 2019/day10.py
+++ 2019/day10.py
@@ -1,53 +1,120 @@
 from numpy import ones,vstack
 from numpy.linalg import lstsq
+import math as m
+from operator import itemgetter
 
-test = """.#..#
-.....
-#####
-....#
-...##"""
+class Asteroid:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.neighbours = []
 
-lines = test.split('\n')
+    def check_if_neighoburs(self, asteroid):
+        dx = (asteroid.x - self.x)
+        dy = (asteroid.y - self.y)
+        if dx == dy == 0:
+            return
+        if dx > 0:
+            sign = 1
+        elif dx < 0:
+            sign = -1
+        try:
+            slope = dy / dx
+        except ZeroDivisionError:
+            if dx == 0 and dy > 0:
+                sign = -1
+                slope = -999
+            else:
+                sign = 1
+                slope = -999
+        self.neighbours.append((slope, sign))
+
+    def get_neighbours(self):
+        return set(self.neighbours)
+
+
+test = """.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##"""
+data = """#.#....#.#......#.....#......####.
+#....#....##...#..#..##....#.##..#
+#.#..#....#..#....##...###......##
+...........##..##..##.####.#......
+...##..##....##.#.....#.##....#..#
+..##.....#..#.......#.#.........##
+...###..##.###.#..................
+.##...###.#.#.......#.#...##..#.#.
+...#...##....#....##.#.....#...#.#
+..##........#.#...#..#...##...##..
+..#.##.......#..#......#.....##..#
+....###..#..#...###...#.###...#.##
+..#........#....#.....##.....#.#.#
+...#....#.....#..#...###........#.
+.##...#........#.#...#...##.......
+.#....#.#.#.#.....#...........#...
+.......###.##...#..#.#....#..##..#
+#..#..###.#.......##....##.#..#...
+..##...#.#.#........##..#..#.#..#.
+.#.##..#.......#.#.#.........##.##
+...#.#.....#.#....###.#.........#.
+.#..#.##...#......#......#..##....
+.##....#.#......##...#....#.##..#.
+#..#..#..#...........#......##...#
+#....##...#......#.###.#..#.#...#.
+#......#.#.#.#....###..##.##...##.
+......#.......#.#.#.#...#...##....
+....##..#.....#.......#....#...#..
+.#........#....#...#.#..#....#....
+.#.##.##..##.#.#####..........##..
+..####...##.#.....##.............#
+....##......#.#..#....###....##...
+......#..#.#####.#................
+.#....#.#..#.###....##.......##.#."""
+
+lines = data.split('\n')
 asteroids = []
+insight = []
 
 for y in range(0, len(lines)):
     for x, elem in enumerate(lines[y]):
         if elem == '.':
             continue
         elif elem == '#':
-            asteroids.append((x, y))
+            asteroids.append(Asteroid(x, y))
         else:
             print("input data not valid")
             break
 
-equations, eq = [], []
-
 for ast in asteroids:
-    (x, y) = ast
-    for i in range(1, len(asteroids)):
-        (x1, y1) = asteroids[i]
-        points = [ast, asteroids[i]]
-        x_coords, y_coords = zip(*points)
-        A = vstack([x_coords,ones(len(x_coords))]).T
-        m, c = lstsq(A, y_coords)[0]
-        m = float("{0:.4f}".format(m))
-        c = float("{0:.4f}".format(c))
-        equations.append((m, c))
+    for i in range(0, len(asteroids)):
+        ast.check_if_neighoburs(asteroids[i])
+    insight.append(len(ast.get_neighbours()))
 
-        # if x == x1 or y == y1:
-        #     a = 0
-        # else:
-        #     a = (x1 - x) / (y1 - y)
-        #     a = float("{0:.4f}".format(a))
-        # b = y - (a * x)
-        # b = a = float("{0:.4f}".format(b))
-        # equations.append((a, b))
-    eq.append(len(set(equations)))
-    equations = []
+print('Part 1: ', max(insight))
+
+target = asteroids[insight.index(max(insight))].neighbours.copy()
+target = list(set(target))
+target.sort(key=itemgetter(0))
+target.sort(key=itemgetter(1),reverse=True)
+print('Part 2: ', asteroids[asteroids[insight.index(max(insight))].neighbours.index(target[199])].x * 100 + asteroids[asteroids[insight.index(max(insight))].neighbours.index(target[199])].y)
 
 
-print(max(eq))
 
-# for i, pair in enumerate(asteroids):
-#     (x, y) = pair
-#     print(pair)
+
